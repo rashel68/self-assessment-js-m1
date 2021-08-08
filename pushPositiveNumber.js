3ebf4028305c1039ed118a429a91f00eccb28572
rashel68 <rashel341068@gmail.com>
2021-08-08 19:56:09 +0600

    pushPositiveNumber added

---
+++ pushPositiveNumber.js
@@ -0,0 +1,10 @@
+let numbers = [1, 5, 4, 3, 5, -4, 7, 6];
+let positveNumber = [];
+for (const element of numbers) {
+    if (element < 0) {
+        break;
+    } else {
+        positveNumber.push(element);
+    }
+}
+console.log(positveNumber);
